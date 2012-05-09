a0ee5a53d491570821a8dd9f922ea15962df1372
richard <richard@olibrius.home>
2012-05-09 14:22:31 +0200

    init

---
+++ workspace/HelloWorld/src/hello/HelloWorld.java
@@ -0,0 +1,15 @@
+package hello;
+
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// ma classe qui affiche tout yes \o/
+		HelloMaker myHelloMaker = new HelloMaker();
+		System.out.println( myHelloMaker.showHello() );
+	}
+
+}
---
+++ workspace/HelloWorld/src/tests/HelloMakerTest.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package tests;
+
+import hello.*;
+import junit.framework.TestCase;
+
+/**
+ * @author richard
+ *
+ */
+public class HelloMakerTest extends TestCase {
+	private HelloMaker myHelloMaker = null;
+	/**
+	 * @param name
+	 */
+	public HelloMakerTest(String name) {
+		super(name);
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+		myHelloMaker = new HelloMaker();
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	/**
+	 * Test method for {@link HelloMaker#showHello()}.
+	 */
+	public void testShowHello() {
+		assertEquals("hello world", myHelloMaker.showHello());
+	}
+
+}
