308caaefa7cc9a76418380945e86c93764e95001
Richard Hitier <richard.hitier@co-libri.org>
2014-10-29 16:59:23 +0100

    new error funct

--- bin/photosort.sh
+++ bin/photosort.sh
@@ -25,32 +25,37 @@ do
     esac
 done
 
-patterns='jpg|JPG'
-
 # For each photo from source directory
 # Read creation date from EXIF
 # Move to new path in destination directory
 
-error()
+function error
 {
+    echo $@
+    return 0
 }
+export -f error
 
 function showdate
 {
     imageFile=$0
-    [ -e $imageFile ] || exit
+    [ -e $imageFile ] || error $imageFile "doesent exist"
     baseImageFile=`basename ${imageFile}`
     exifFile=${baseImageFile}.exif
-    exif -t DateTimeOriginal ${imageFile} 2>/dev/null > ${exifFile}
+    exif -t DateTimeOriginal ${imageFile} 2>/dev/null > ${exifFile} || error "No exif for "${imageFile}
     destDir=`grep Value ${exifFile} | sed -e 's#.*\(....\):\(..\):.. .*#\1/\2#'`
     rm ${exifFile}
     [ -d ${destDir} ] || mkdir -p ${destDir}
     destImageFile=${destDir}/${baseImageFile}
-    [ -e ${destImageFile} ] && exit
+    [ -e ${destImageFile} ] && error ${destImageFile} exists
     cp ${imageFile} ${destImageFile}
 
 }
 
 export -f showdate
 
+
+patterns='jpg|JPG'
 find -regextype posix-extended -regex '.*\.(jpg|png|JPG|JPEG|CR2)' -exec bash -c showdate '{}' \;
+
+# vim: tw=0
