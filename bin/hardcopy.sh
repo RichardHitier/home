dedf02550ce930d0edb6da12bace8f29f45eb2cd
Richard Hitier <richard.hitier@co-libri.org>
2015-11-17 14:42:07 +0100

    rearrange lines

--- bin/hardcopy.sh
+++ bin/hardcopy.sh
@@ -17,15 +17,12 @@ help()
 .END
 }
 
-which vim 2>&1 > /dev/null || { help "please, install vim" ; exit ;}
-which ps2pdf 2>&1 > /dev/null  || { help "please, install ps2pdf" ; exit ;}
-[ 'x' = 'x'$1 ] && help "please, give file as arg" && exit
-
 input=$1
 
+which vim 2>&1 > /dev/null || { help "please, install vim" ; exit ;}
+which ps2pdf 2>&1 > /dev/null  || { help "please, install ps2pdf" ; exit ;}
+[ 'x' = 'x'$input ] && help "please, give file as arg" && exit
 [  -e $input ] || { help "wrong file "$input; exit; }
-
-#exit
 
 baseInput=$(pwd)/$(basename ${input})
 outputPS=${baseInput}.ps
@@ -35,6 +32,7 @@ vim -c 'hardcopy >  '$outputPS -c quit $input
 
 ps2pdf  $outputPS $outputPDF
 rm $outputPS
-#
+
 output=$outputPDF
+clear
 echo 'Ok, written to '$output
