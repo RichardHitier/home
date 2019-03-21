4f2883e2e7aaa828700f8289209bfd0e7d5ea24d
Richard Hitier <richard.hitier@co-libri.org>
2019-03-21 09:54:25 +0100

    Allow int keyboard

--- bin/setMyDvorak.sh
+++ bin/setMyDvorak.sh
@@ -3,5 +3,5 @@
 # change keyboard mapping to a personalized dvorak with
 # compose key to allow accentuated chars
 
-/usr/bin/setxkbmap fr
+/usr/bin/setxkbmap us_intl
 /usr/bin/xmodmap ~/config/misc/myDvorak.xmod 
