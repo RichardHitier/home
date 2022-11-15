25e5b5aa33d3bf22f3ff8d2a84c1a26fc4c9e82f
Richard Hitier <hitier.richard@gmail.com>
2022-11-15 09:22:12 +0100

    Add shell aliases

---
+++ .zsh_custom/aliases.zsh
@@ -0,0 +1,10 @@
+
+alias findname='find ./ -name'
+alias qsdf=$HOME'/bin/xmodmapgenerator'
+alias aoeui='setxkbmap fr'
+alias aoeu=qsdf
+alias asdf=qsdf
+alias qsdfg='sudo loadkeys ~/config/misc/mydvorak-console'
+alias showip="sudo ifconfig eth0  | grep 'inet adr' | sed -e 's/^.*adr:\([^ ]*\).*$/\1/'"
+alias acs="apt-cache search"
+alias sagi="sudo apt-get install"
