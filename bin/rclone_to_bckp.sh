9fe1f93182e792c48b028ae8e20f33ba4d478948
Richard Hitier <hitier.richard@gmail.com>
2025-11-04 18:07:59 +0100

    Change rclone script log format

--- bin/rclone_to_bckp.sh
+++ bin/rclone_to_bckp.sh
@@ -5,13 +5,20 @@
 
 LOG_FILE="$HOME/rclone-sync.log"
 
-echo "=== Début synchronisation $(date) ===" >> "$LOG_FILE"
+echo "=== Début synchronisation $(date) ===\n" > "$LOG_FILE"
 
-rclone sync --progress rhi.txt  gdrive:backup/
 
-#rclone sync --progress 00PRO gdrive:backup/00PRO 2>&1 | tee -a "$LOG_FILE"
-#rclone sync --progress 03COMMON gdrive:backup/03COMMON 2>&1 | tee -a "$LOG_FILE"
-#rclone sync --progress Bureau gdrive:backup/Bureau 2>&1 | tee -a "$LOG_FILE"
+echo "==  Synchronisation de ~/00PRO ==" >> "$LOG_FILE"
+echo "==  -------------------------- ==\n" >> "$LOG_FILE"
+rclone sync --log-level INFO  00PRO gdrive:backup/00PRO 2>&1 | tee -a "$LOG_FILE"
+
+echo "==  Synchronisation de ~/00COMMON ==" >> "$LOG_FILE"
+echo "==  ----------------------------- ==\n" >> "$LOG_FILE"
+rclone sync --log-level INFO  03COMMON gdrive:backup/03COMMON 2>&1 | tee -a "$LOG_FILE"
+
+echo "==  Synchronisation de ~/Bureau ==" >> "$LOG_FILE"
+echo "==  --------------------------- ==\n" >> "$LOG_FILE"
+rclone sync --log-level INFO  Bureau gdrive:backup/Bureau 2>&1 | tee -a "$LOG_FILE"
 
 echo "=== Fin synchronisation $(date) ===" >> "$LOG_FILE"
 echo "" >> "$LOG_FILE"
