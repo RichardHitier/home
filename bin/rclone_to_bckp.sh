9391c6e0d2d43d6109c5bbe424f766d3710835d1
Richard Hitier <hitier.richard@gmail.com>
2026-03-07 12:50:32 +0100

    Refactor rclone for flexibility

--- bin/rclone_to_bckp.sh
+++ bin/rclone_to_bckp.sh
@@ -10,24 +10,26 @@ echo -e "=== Début synchronisation $(date) ===\n" > "$LOG_FILE"
 # Vérification de la connexion réseau
 echo -e "\n== Vérification réseau ==" >> "$LOG_FILE"
 echo -e "== ------------------- ==\n" >> "$LOG_FILE"
+
 if ping -c 1 google.com &>/dev/null; then
-    echo -e "Connexion réseau OK" >> "$LOG_FILE"
+    echo "Connexion réseau OK" >> "$LOG_FILE"
 else
-    echo -e "Erreur : pas de connexion réseau, synchronisation annulée." >> "$LOG_FILE"
+    echo "Erreur : pas de connexion réseau, synchronisation annulée." >> "$LOG_FILE"
     exit 1
 fi
 
-echo -e "\n==  Synchronisation de ~/00PRO ==" >> "$LOG_FILE"
-echo -e "==  -------------------------- ==\n" >> "$LOG_FILE"
-rclone sync --log-level INFO  00PRO gdrive:backup/00PRO 2>&1 | tee -a "$LOG_FILE"
+DIRS=(
+    "00PRO"
+    "03COMMON"
+    "Bureau"
+)
 
-echo -e "\n==  Synchronisation de ~/00COMMON ==" >> "$LOG_FILE"
-echo -e "==  ----------------------------- ==\n" >> "$LOG_FILE"
-rclone sync --log-level INFO  03COMMON gdrive:backup/03COMMON 2>&1 | tee -a "$LOG_FILE"
+for dir in "${DIRS[@]}"; do
+    echo -e "\n== Synchronisation de ~/$dir ==" >> "$LOG_FILE"
+    echo -e "== --------------------------- ==\n" >> "$LOG_FILE"
 
-echo -e "\n==  Synchronisation de ~/Bureau ==" >> "$LOG_FILE"
-echo -e "==  --------------------------- ==\n" >> "$LOG_FILE"
-rclone sync --log-level INFO  Bureau gdrive:backup/Bureau 2>&1 | tee -a "$LOG_FILE"
+    rclone sync --log-level INFO "$dir" "gdrive:backup/$dir" 2>&1 | tee -a "$LOG_FILE"
+done
 
 echo -e "\n=== Fin synchronisation $(date) ===" >> "$LOG_FILE"
-echo -e "" >> "$LOG_FILE"
+echo "" >> "$LOG_FILE"
